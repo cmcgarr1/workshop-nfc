3f1ee3db1fc5436d4dd7fa653f06b1dcf99542fb
Connor Mcgarr <connormcgarr@Connors-MacBook-Air.local>
2026-06-18 11:27:39 -0500

    Fix env vars / API error handling

--- pages/api/items.js
+++ pages/api/items.js
@@ -2,7 +2,14 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY
+)
+
+
+const
+ supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY
 )
 
 export default async function handler(req, res) {
